c05c2aabffb1b0496d78ef452816c3d93c49548e
Lucas Åström <lucasas@kth.se>
2016-01-29 13:48:52 +0100

    add and remove users

--- src/tokig_server.erl
+++ src/tokig_server.erl
@@ -3,7 +3,10 @@
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
--export([start_link/3]).
+-export([start_link/3,
+         add_user/2,
+         remove_user/2]).
+
 -behaviour(gen_server).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
@@ -35,12 +38,22 @@ start_link_works_if_memberships_are_valid_test() ->
     ?assertNot(undefined == whereis(?MODULE)).
 -endif.
 
+add_user(Pid, User) ->
+    gen_server:cast(Pid, {add_user, User}).
+
+remove_user(Pid, User) ->
+    gen_server:cast(Pid, {remove_user, User}).
+
 init(Args) ->
     {ok, undefined}.
 
 handle_call(_Request, _From, State) ->
     {reply, {error, unknown_call}, State}.
 
+handle_cast({add_user, User}, #state{users = Users} = State) ->
+    {noreply, State#state{users = do_add_user(User, Users)}};
+handle_cast({remove_user, User}, #state{users = Users} = State) ->
+    {noreply, State#state{users = do_remove_user(User, Users)}};
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
@@ -55,3 +68,21 @@ code_change(_OldVsn, State, _Extra) ->
 
 is_membership_valid(Users, Groups, {User, Group}) ->
     lists:member(User, Users) andalso lists:member(Group, Groups).
+
+do_add_user(User, Users) ->
+    [User|Users].
+
+-ifdef(TEST).
+do_add_user_test() ->
+    NewUsers = do_add_user(new, [old, someone]),
+    ?assert(lists:member(new, NewUsers)).
+-endif.
+
+do_remove_user(User, Users) ->
+    lists:delete(User, Users).
+
+-ifdef(TEST).
+do_remove_user_test() ->
+    NewUsers = do_remove_user(old, [old, someone]),
+    ?assertNot(lists:member(old, NewUsers)).
+-endif.
